59130b71f84609d7d4811da556df217c990c5588
LE7ELS001 <95457016+LE7ELS001@users.noreply.github.com>
2026-01-28 18:48:30 -0500

    update week 2 project
    copy teacher's example and modify it

---
+++ Projects/Week 2 Project/Assets/Scripts/BuildingDropperScript.cs
@@ -0,0 +1,57 @@
+using System.Collections;
+using UnityEngine;
+
+public class BuildingDropperScript : MonoBehaviour
+{
+    public GameObject building;
+
+    public Sprite[] buildingSprites;
+
+    public float startInterval = 1.5f;
+    public float minInterval = 0.5f;
+    public float intervalDecay = 0.03f;
+    private float currentInterval;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+
+        currentInterval = startInterval;
+        // Start the Drop Routine
+        StartCoroutine(SpawnBuilding());
+    }
+
+    private IEnumerator SpawnBuilding()
+    {
+        // which building are we going to drop!?
+        int buildingNumber = Random.Range(0, buildingSprites.Length);
+
+        // is it a mall? 
+        string buildingType;
+        if (buildingNumber == 0)
+        {
+            buildingType = "Mall";
+        }
+        else
+        {
+            buildingType = "NotaMall";
+        }
+
+        // where are we going to drop it?
+        float x = Random.Range(-8f, 8f);
+        Vector3 buildingPos = new Vector3(x, 8, 0);
+
+        // Create the Building
+        GameObject currentBuilding = Instantiate(building, buildingPos, transform.rotation);
+        // Get the SpriteRenderer component
+        SpriteRenderer sr = currentBuilding.GetComponent<SpriteRenderer>();
+        // set the sprite
+        sr.sprite = buildingSprites[buildingNumber];
+        // Set the Tag
+        currentBuilding.tag = buildingType;
+
+        yield return new WaitForSeconds(currentInterval);
+        currentInterval = Mathf.Max(minInterval, currentInterval - intervalDecay);
+        StartCoroutine(SpawnBuilding());
+    }
+}
